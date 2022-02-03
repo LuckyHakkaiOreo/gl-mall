3ca4b06f0a8b606229629544bc3f030f7e02d69f
LuckyHakkaiOreo <city1005>
2022-02-03 18:56:34 +0800

    1.init

---
+++ glmall-member/src/test/java/com/winster/glmall/glmallmember/GlmallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winster.glmall.glmallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlmallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ glmall-order/src/test/java/com/winster/glmall/glmallorder/GlmallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winster.glmall.glmallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlmallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
